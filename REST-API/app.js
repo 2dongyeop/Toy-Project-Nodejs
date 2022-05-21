ab7c85f710a244136c8a765cc6170024aa8f74a4
leedongyeop <20181676@edu.hanbat.ac.kr>
2022-05-21 20:50:27 +0900

    로그인을 구현하는 간단한 코드

---
+++ REST-API/app.js
@@ -0,0 +1,82 @@
+const fs = require('fs');
+const express = require("express");
+const DB = require('./REST-API-db.json');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.get('/members', (req, res, next) => {
+    res.status(200).send({
+        message: "GET 요청이 성공적으로 수행됨"
+    });
+    res.send()
+});
+
+
+fs.readFile('./REST-API-db.json', 'utf8', (error, jsonFile) => {
+    if (error) return console.log(error);
+    const jsonData = JSON.parse(jsonFile);
+
+    const members = jsonData.members;
+    members.forEach(member => {
+        console.log(member);
+        console.log(member.name);
+    });
+
+    app.post('/members', (req, res, next) => {
+        if (members.name === req.body.name) {
+            if (members.password === req.body.password) {
+                res.status(200).send("Login: Success");
+            } else {
+                res.status(404).send("Login: Fail");
+            }
+        }
+    });
+});
+
+app.listen(3000, () => {
+    console.log("starting server at port 3000..");
+});
+
+
+
+
+
+//
+// fs.readFile('./REST-API-db.json', 'utf8', (error, jsonFile) => {
+//     if (error) return console.log(error);
+//     const jsonData = JSON.parse(jsonFile);
+//
+//     const members = jsonData.members;
+//     members.forEach(member => {
+//         console.log(member);
+//         console.log(member.name);
+//     });
+// });
+
+// app.post('/members', (req, res) => {
+//     console.log(req.body);
+//
+//     fs.readFile('./REST-API-db.json', 'utf8', (error, jsonFile) => {
+//         if (error) return console.log(error);
+//         const jsonData = JSON.parse(jsonFile);
+//
+//         const members = jsonData.members;
+//         members.forEach(member => {
+//             // console.log(member);
+//             // console.log(member.name);
+//         });
+//         const memberName =
+//         if (members.name === req.name) {
+//             if (members.password === req.password) {
+//                 res.send("Login Success");
+//             }
+//         }
+//         else {
+//             res.send("Login Fail");
+//         }
+//     });
+// });
